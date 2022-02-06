56f0adf81834938df2b8766a4c0449dcbc11cef6
AnneliKlamas <annelik98@gmail.com>
2022-02-06 23:21:24 +0200

    added endpoint for deleting all data

--- src/main/java/com/example/farm/controller/FarmDataController.java
+++ src/main/java/com/example/farm/controller/FarmDataController.java
@@ -1,12 +1,15 @@
 package com.example.farm.controller;
 
 import com.example.farm.dto.FarmDataDto;
+import com.example.farm.repository.FarmDataRepository;
 import com.example.farm.service.FarmDataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -15,9 +18,15 @@ public class FarmDataController {
 
     @Autowired
     private FarmDataService farmDataService;
+    @Autowired
+    private FarmDataRepository farmDataRepository;
 
     @GetMapping("")
     private List<FarmDataDto> getFarmData() {
         return farmDataService.getAll();
     }
+
+    @DeleteMapping("/delete")
+    @Transactional
+    void deleteAllData(){farmDataRepository.deleteAll();}
 }
